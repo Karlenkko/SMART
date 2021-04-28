d2447ea5440413d2ac719468ef4f80a3bd174d96
Karlenkko <48716383+Karlenkko@users.noreply.github.com>
2021-04-28 18:05:17 +0200

    update index api

--- blueprints/index/index.py
+++ blueprints/index/index.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from model import Farm, Product
+from model import Farm, Product, Order, Request, User
 from flask import abort, request, jsonify
 import json
 
@@ -8,12 +8,58 @@ index_bp = Blueprint('index', __name__)
 
 @index_bp.route('/index/getAllFarmOrders/', methods=['GET'])
 def getAllFarmOrders():
-    return 'Get All Farm Orders'
+    orders = Order.query.all()
+    farms = Farm.query.all()
+    farmuserids = {}
+    res = []
+    for farm in farms:
+        farmuserids[farm.userid] = farm
+    for order in orders:
+        if order.ownerid in farmuserids.keys():
+            requests = Request.query.filter(Request.orderid == order.id)
+            count = 0
+            for req in requests:
+                if len(req.volunteertime) > 0:
+                    count = count + 1;
+            totalMembers = len(order.requestlist.split(',')) - 1
+            res.append({
+                "farmId" : farmuserids[order.ownerid].id,
+                "farmName" : farmuserids[order.ownerid].name,
+                "farmAddress" : farmuserids[order.ownerid].address,
+                "url" : farmuserids[order.ownerid].photourl,
+                "totalMembers" : totalMembers,
+                "totalVolunteers" : count
+            })
+    return jsonify(res), 200
 
 
 @index_bp.route('/index/getAllUserOrders/', methods=['GET'])
 def getAllUserOrders():
-    return 'Get All User Orders'
+    orders = Order.query.all()
+    users = User.query.all()
+    userdict = {}
+    res = []
+    for user in users:
+        if user.type == "user":
+            userdict[user.id] = user
+    for order in orders:
+        if order.ownerid in userdict.keys():
+            totalMembers = len(order.requestlist.split(',')) - 1
+            points = order.entrepotlist.split(';')
+            res.append({
+                "userId" : order.ownerid,
+                "userName" : userdict[order.ownerid].name,
+                "date" : order.time,
+                "state" : order.state,
+                "description" : order.description,
+                "totalCandidate" : totalMembers,
+                "depart" : points[0],
+                "destination" : points[1],
+                "url" : userdict[order.ownerid].photourl,
+                "tel" : userdict[order.ownerid].mobile
+            })
+
+    return jsonify(res), 200
 
 
 @index_bp.route('/index/getAllFarms/', methods=['GET'])
