6c200730becd641135bb64e6797eaebb9856f3b6
Karlenkko <48716383+Karlenkko@users.noreply.github.com>
2021-05-05 16:24:10 +0200

    add confirm API

--- blueprints/user/user.py
+++ blueprints/user/user.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 from flask import abort, request, jsonify
-from model import User, Order, Request, Volunteer
+from model import User, Order, Request, Volunteer, Coupon
 from exts import db
 from sqlalchemy import and_
 user_bp = Blueprint('user', __name__)
@@ -46,11 +46,37 @@ def confirmRequest():
         abort(400)
     else:
         order = Order.query.filter(Order.id == request.args.get('orderId')).first()
+        print(order)
         req = Request.query.filter(and_(Request.orderid == request.args.get('orderId'), Request.userid == request.args.get('userId'))).first()
+        print(req)
+        cl = ""
+        if not order.confirmlist == None:
+            cl = str(order.confirmlist)
         db.session.query(Order).filter(Order.id == request.args.get('orderId')).update({
-            "confirmlist": order.confirmlist + str(req.id) + ","
+            "confirmlist": cl + str(req.id) + ","
         })
         db.session.commit()
+        newOrder = Order.query.filter(Order.id == request.args.get('orderId')).first()
+        if str(newOrder.requestlist) == str(newOrder.confirmlist):
+            db.session.query(Order).filter(Order.id == request.args.get('orderId')).update({"state" : 10})
+            db.session.query(Volunteer).filter(Volunteer.orderid == request.args.get('orderId')).update({"accept" : 10})
+            db.session.commit()
+            volunteers = Volunteer.query.filter(Volunteer.orderid == request.args.get('orderId')).all()
+            for vol in volunteers:
+                user = User.query.filter(User.id == vol.userid).first()
+                actual = 0
+                if user.volunteeractual >= 96:
+                    actual = int(user.volunteeractual) + 4 - 100
+                    count = Coupon.query.count()
+                    coupon = Coupon(int(count), int(user.id), 5)
+                    db.session.add(coupon)
+                else:
+                    actual = int(user.volunteeractual) + 4
+                db.session.query(User).filter(User.id == vol.userid).update({
+                    "volunteertotal" : int(user.volunteertotal) + 4,
+                    "volunteeractual" : actual
+                })
+                db.session.commit()
         return 'confirmed', 201
 
 @user_bp.route('/user/getMyRequests/', methods=['GET'])
